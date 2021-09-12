de90b97e57f4e2dbbc847f1582392ca8a3fd7e75
ulvi95 <bajaraniulvi@gmail.com>
2021-09-11 22:38:52 -0500

    Adding Results
    Added "Results" folder.

---
+++ PyGraphCreation/graph_creation_random_forest_hundredfifty_computation.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot
+
+
+def get_variable_name(obj, namespace):
+    return [name for name in namespace if namespace[name] is obj]
+
+def get_list_of_files(folder_with_results):
+    results = []
+    for file in os.listdir(folder_with_results):
+        if file.startswith("Random_Forest_OneHundredFifty") and file.endswith(".csv"):
+            results.append(folder_with_results+file)
+    return results
+
+
+if __name__ == "__main__":
+    
+    folder_path = os.getcwd() + os.path.sep
+    folder_with_results = os.path.dirname(os.getcwd()) + os.sep + "Results" + os.sep
+    csv_files = get_list_of_files(folder_with_results)
+    
+    Random_Forest_OneHundredFifty_results_no_duplicates_all_results_gini = [i for i in csv_files if "no_duplicates" in i and "gini" in i and "_2" not in i and "M_F" not in i]
+    Random_Forest_OneHundredFifty_results_no_duplicates_all_2_results_gini = [i for i in csv_files if "no_duplicates" in i and "gini" in i and "_2" in i and "M_F" not in i]
+    Random_Forest_OneHundredFifty_results_no_duplicates_all_results_entropy = [i for i in csv_files if "no_duplicates" in i and "entropy" in i and "_2" not in i and "M_F" not in i]
+    Random_Forest_OneHundredFifty_results_no_duplicates_all_2_results_entropy = [i for i in csv_files if "no_duplicates" in i and "entropy" in i and "_2" in i and "M_F" not in i]
+    Random_Forest_OneHundredFifty_results_no_duplicates_m_f_only_gini = [i for i in csv_files if "no_duplicates" in i and "gini" in i and "_2" not in i and "M_F" in i]
+    Random_Forest_OneHundredFifty_results_no_duplicates_m_f_only_2_gini = [i for i in csv_files if "no_duplicates" in i and "gini" in i and "_2" in i and "M_F" in i]
+    Random_Forest_OneHundredFifty_results_no_duplicates_m_f_only_entropy = [i for i in csv_files if "no_duplicates" in i and "entropy" in i and "_2" not in i and "M_F" in i]
+    Random_Forest_OneHundredFifty_results_no_duplicates_m_f_only_2_entropy = [i for i in csv_files if "no_duplicates" in i and "entropy" in i and "_2" in i and "M_F" in i]
+    Random_Forest_OneHundredFifty_results_with_duplicates_all_results_gini = [i for i in csv_files if "no_duplicates" not in i and "gini" in i and "_2" not in i and "M_F" not in i]
+    Random_Forest_OneHundredFifty_results_with_duplicates_all_2_results_gini = [i for i in csv_files if "no_duplicates" not in i and "gini" in i and "_2" in i and "M_F" not in i]
+    Random_Forest_OneHundredFifty_results_with_duplicates_all_results_entropy = [i for i in csv_files if "no_duplicates" not in i and "entropy" in i and "_2" not in i and "M_F" not in i]
+    Random_Forest_OneHundredFifty_results_with_duplicates_all_2_results_entropy = [i for i in csv_files if "no_duplicates" not in i and "entropy" in i and "_2" in i and "M_F" not in i]
+    Random_Forest_OneHundredFifty_results_with_duplicates_m_f_only_gini = [i for i in csv_files if "no_duplicates" not in i and "gini" in i and "_2" not in i and "M_F" in i]
+    Random_Forest_OneHundredFifty_results_with_duplicates_m_f_only_2_gini = [i for i in csv_files if "no_duplicates" not in i and "gini" in i and "_2" in i and "M_F" in i]
+    Random_Forest_OneHundredFifty_results_with_duplicates_m_f_only_entropy = [i for i in csv_files if "no_duplicates" not in i and "entropy" in i and "_2" not in i and "M_F" in i]
+    Random_Forest_OneHundredFifty_results_with_duplicates_m_f_only_2_entropy = [i for i in csv_files if "no_duplicates" not in i and "entropy" in i and "_2" in i and "M_F" in i]
+    
+    condition_to_be_tested = [Random_Forest_OneHundredFifty_results_no_duplicates_all_results_gini,
+                           Random_Forest_OneHundredFifty_results_no_duplicates_all_2_results_gini,
+                           Random_Forest_OneHundredFifty_results_no_duplicates_all_results_entropy,
+                           Random_Forest_OneHundredFifty_results_no_duplicates_all_2_results_entropy,
+                           Random_Forest_OneHundredFifty_results_no_duplicates_m_f_only_gini,
+                           Random_Forest_OneHundredFifty_results_no_duplicates_m_f_only_2_gini,
+                           Random_Forest_OneHundredFifty_results_no_duplicates_m_f_only_entropy,
+                           Random_Forest_OneHundredFifty_results_no_duplicates_m_f_only_2_entropy,
+                           Random_Forest_OneHundredFifty_results_with_duplicates_all_results_gini,
+                           Random_Forest_OneHundredFifty_results_with_duplicates_all_2_results_gini,
+                           Random_Forest_OneHundredFifty_results_with_duplicates_all_results_entropy,
+                           Random_Forest_OneHundredFifty_results_with_duplicates_all_2_results_entropy,
+                           Random_Forest_OneHundredFifty_results_with_duplicates_m_f_only_gini,
+                           Random_Forest_OneHundredFifty_results_with_duplicates_m_f_only_2_gini,
+                           Random_Forest_OneHundredFifty_results_with_duplicates_m_f_only_entropy,
+                           Random_Forest_OneHundredFifty_results_with_duplicates_m_f_only_2_entropy]
+
+
+    for condition in condition_to_be_tested:
+        if len(condition) == 0:
+            continue
+        fig, ax = matplotlib.pyplot.subplots()
+        fig.set_figheight(15)
+        fig.set_figwidth(15)
+
+        for condition_check in condition:
+            list1 = pd.read_csv(condition_check,header=None).values.tolist()
+            if "_names_only" in condition_check:
+                ax.plot(range(1, 100), list1[0], color='r', label='Names only')
+            elif "_surnames_only" in condition_check:
+                ax.plot(range(1, 100), list1[0], color='g', label='Surnames only')
+            elif "surname_name" in condition_check:
+                ax.plot(range(1, 100), list1[0], color='b', label='Names and Surnames')
+
+        ax.set_xticks(np.arange(0, 105, 5))
+        ax.set_yticks(np.arange(0, 1.1, 0.1))
+        ax.set_xlabel("Training data size in percents", fontsize=16)
+        ax.set_ylabel("Accuracy fraction (percentage/100)", fontsize=16)
+        ax.set_yticklabels(("0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0"), fontsize=16)
+        ax.set_xticklabels(("0", "5", "10", "15", "20", "25", "30", "35",
+                            "40", "45", "50", "55", "60", "65", "70", "75",
+                            "80", "85", "90", "95", "100"), fontsize=16)
+        ax.legend(fontsize=16)
+        text = get_variable_name(condition, locals())
+        ax.set_title(text[0], fontsize=16)
+        image_with_plotters = folder_with_results + text[0] + ".png"
+        matplotlib.pyplot.savefig(image_with_plotters, bbox_inches=ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted()).expanded(1.3, 1.2))
---
+++ Py_RandomForestFiles/random_forest_twohundred_computation_with_duplicates_all_names2_entropy_name.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+import datetime
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn import preprocessing
+
+
+def get_variable_name(obj, namespace):
+    return [name for name in namespace if namespace[name] is obj]
+
+def check_existing_file(file):
+    if os.path.exists(file):
+        text = "\"" + file + "\" exists."
+        print(text)
+        file_results_log = open("logFile10.txt","a+")
+        file_results_log.write(text + "\n")
+        file_results_log.close()
+    else:
+        text = "Error: \"" + file + "\" doesn't exist."
+        print(text)
+        file_results_log = open("logFile10.txt","a+")
+        file_results_log.write(text + "\n")
+        file_results_log.close()
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    
+    folder_path = os.getcwd() + os.path.sep
+    folder_path_one_level_up = os.path.dirname(os.getcwd())
+    
+    textfile_log = folder_path + "logFile10.txt"
+    textfile_log_results = folder_path + "Results_logFile10.txt"
+
+    if os.path.exists(textfile_log):
+        file_results_log = open("logFile10.txt","a+")
+        file_results_log.write(str(datetime.datetime.now()) + "\n")
+        file_results_log.close()
+    else:
+        file_results_log = open("logFile10.txt","w+")
+        file_results_log.write(str(datetime.datetime.now()) + "\n")
+        file_results_log.close()
+        
+    if os.path.exists(textfile_log_results):
+        file_results_log = open("Results_logFile10.txt","a+")
+        file_results_log.write(str(datetime.datetime.now()) + "\n")
+        file_results_log.close()
+    else:
+        file_results_log = open("Results_logFile10.txt","w+")
+        file_results_log.write(str(datetime.datetime.now()) + "\n")
+        file_results_log.close()
+        
+    csv_with_all_surnames = folder_path_one_level_up + os.sep + "Results" + os.sep + "surnames_labeled_all_2.csv"
+    csv_Random_Forest_results_entropy = folder_path_one_level_up + os.sep + "Results" + os.sep + "Random_Forest_TwoHundred_results_entropy_names_only_2.csv"
+
+    check_existing_file(csv_with_all_surnames)
+    
+    total_accuracy_scores = []
+    
+
+    data_to_be_tested = pd.read_csv(csv_with_all_surnames,header=None)
+    column_names = ['Surname', 'Name', 'Gender']
+    data_to_be_tested.columns=column_names
+    
+    names_only = data_to_be_tested.drop(['Surname', 'Gender'],axis=1)
+    genders=data_to_be_tested['Gender']
+    
+    del(data_to_be_tested)
+    
+    names_only = preprocessing.OneHotEncoder(dtype=np.uint8).fit_transform(names_only)
+    genders=genders.values.reshape(-1,1)
+    genders=preprocessing.OneHotEncoder().fit_transform(genders).toarray()
+
+    accuracy_score = []
+    number_of_trees=200
+    for test_percent in range(99, 0, -1):
+        x_train, x_test, y_train, y_test = train_test_split(names_only, genders, test_size=(test_percent/100), shuffle=False)
+        classifier_random_forest = RandomForestClassifier(n_estimators=number_of_trees, n_jobs=-1, criterion='entropy')
+        classifier_random_forest = classifier_random_forest.fit(x_train, y_train)
+        y_pred_random_forest = classifier_random_forest.predict(x_test)
+        random_forest_accuracy_score = round(metrics.accuracy_score(y_test, y_pred_random_forest), 3)
+        text = "Accuracy of Random Forest with " + str(number_of_trees) + " trees, splitting criterion 'entropy' for " + str(get_variable_name(names_only, locals())[0]) + " with training data fraction " + str(100-test_percent) + "% in " + str(csv_with_all_surnames.split(os.sep)[-1]) + " is: " + str(random_forest_accuracy_score)
+        print(text)
+        file_results_log = open("logFile10.txt","a+")
+        file_results_log.write(text + "\n")
+        file_results_log.close()
+        accuracy_score.append(random_forest_accuracy_score)
+    accuracy_score_max = "Maximum accuracy of Random Forest with " + str(number_of_trees) + " trees, splitting criterion 'entropy' for " + str(get_variable_name(names_only, locals())[0]) + " in " + str(csv_with_all_surnames.split(os.sep)[-1]) + " is: " + str(max(accuracy_score)) + " with training data fraction " + str(accuracy_score.index(max(accuracy_score))+1) + "%"
+    print(accuracy_score_max)
+    file_results_log = open("logFile10.txt","a+")
+    file_results_log.write(accuracy_score_max + "\n")
+    file_results_log.close()
+    file_results_log2 = open("Results_logFile10.txt","a+")
+    file_results_log2.write(accuracy_score_max + "\n")
+    file_results_log2.close()
+    accuracy_score_min = "Minimum accuracy of Random Forest with " + str(number_of_trees) + " trees, splitting criterion 'entropy' for " + str(get_variable_name(names_only, locals())[0]) + " in " + str(csv_with_all_surnames.split(os.sep)[-1]) + " is: " + str(min(accuracy_score)) + " with training data fraction " + str(accuracy_score.index(min(accuracy_score))+1) + "%"
+    print(accuracy_score_min)
+    file_results_log = open("logFile10.txt","a+")
+    file_results_log.write(accuracy_score_min + "\n")
+    file_results_log.close()
+    file_results_log2 = open("Results_logFile10.txt","a+")
+    file_results_log2.write(accuracy_score_min + "\n")
+    file_results_log2.close()
+    accuracy_score_average = "Average accuracy of Random Forest with " + str(number_of_trees) + " trees, splitting criterion 'entropy' for " + str(get_variable_name(names_only, locals())[0]) + " in " + str(csv_with_all_surnames.split(os.sep)[-1]) + " is: " + str(round((sum(accuracy_score) / len(accuracy_score)),2))
+    print(accuracy_score_average)
+    file_results_log = open("logFile10.txt","a+")
+    file_results_log.write(accuracy_score_average + "\n")
+    file_results_log.close()
+    file_results_log2 = open("Results_logFile10.txt","a+")
+    file_results_log2.write(accuracy_score_average + "\n")
+    file_results_log2.close()
+    total_accuracy_scores.append(accuracy_score)
+
+    (pd.DataFrame(total_accuracy_scores)).to_csv(csv_Random_Forest_results_entropy, header=False, index=False)
